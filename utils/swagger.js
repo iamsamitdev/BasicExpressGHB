648b21a0290f1f3ecdd0a6620ed0beafbd33caa3
Samit Koyom <samitkoyom@gmail.com>
2024-10-18 14:36:10 +0700

    add schemas

--- utils/swagger.js
+++ utils/swagger.js
@@ -22,6 +22,63 @@ const options = {
           bearerFormat: 'JWT',
         },
       },
+      schemas: {
+        User: {
+          type: 'object',
+          properties: {
+            id: {
+              type: 'integer',
+              description: 'The user ID',
+              example: 1,
+            },
+            username: {
+              type: 'string',
+              description: 'The user\'s username',
+              example: 'john_doe',
+            },
+            password: {
+              type: 'string',
+              description: 'The user\'s hashed password',
+              example: '$2a$12$abcdefghijk',
+            },
+            fullname: {
+              type: 'string',
+              description: 'The user\'s full name',
+              example: 'John Doe',
+            },
+            email: {
+              type: 'string',
+              description: 'The user\'s email address',
+              example: 'john.doe@example.com',
+            },
+            tel: {
+              type: 'string',
+              description: 'The user\'s telephone number',
+              example: '0812345678',
+            },           
+          },
+        },
+        Product: {
+          type: 'object',
+          properties: {
+            id: {
+              type: 'integer',
+              description: 'The product ID',
+              example: 1,
+            },
+            name: {
+              type: 'string',
+              description: 'The product name',
+              example: 'iPhone 12 Pro Max',
+            },
+            price: {
+              type: 'integer',
+              description: 'The price of product',
+              example: '1000',
+            }
+          },
+        },
+      },
     },
     security: [
       {
